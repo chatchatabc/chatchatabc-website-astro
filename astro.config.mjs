9de3c7fc5646f27610912f1c4f976b4cc6d39347
BoJoNVi <bonjomontes@gmail.com>
2023-01-05 09:53:21 +0800

    Installed astrojs/image and mdx package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(),
+    mdx(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
 });
