ab2d397eafa6386b6b9569e8dcfaa71398a88372
BoJoNVi <bonjomontes@gmail.com>
2023-01-05 13:35:32 +0800

    Implemented astro imagetools in homepage

--- astro-imagetools.config.mjs
+++ astro-imagetools.config.mjs
@@ -3,9 +3,4 @@ import { defineConfig } from "astro-imagetools/config";
 export default defineConfig({
   format: ["webp", "jpg"],
   layout: "fill",
-  formatOptions: {
-    webp: {
-      quality: 50,
-    },
-  },
 });
