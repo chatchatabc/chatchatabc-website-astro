d6ddd419fabff8311ecfde869f83ff957c1bff45
BoJoNVi <bonjomontes@gmail.com>
2023-01-06 14:03:30 +0800

    Implemented responsive design for the outsourcing

--- src/components/OutSourcing.tsx
+++ src/components/OutSourcing.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function OutSourcing() {
   const positions = [
@@ -12,40 +12,55 @@ function OutSourcing() {
     if (selected) setSelected(null);
     else setSelected(index);
   };
+
+  useEffect(() => {
+    document.getElementById("card-focus")?.focus();
+  });
+
   return (
     <div>
+      {/* Card Container */}
       <ul
-        className={`flex px-16 relative justify-center ${
-          selected ? "" : "gap-8"
-        }`}
+        className={`flex px-8 relative justify-center flex-wrap gap-8 ${
+          selected ? "lg:gap-0" : ""
+        } lg:flex-nowrap lg:px-16`}
       >
         {positions.map((position, index) => {
           index += 1;
+          const isSelected = selected === index;
           return (
             <li
               key={position + index}
+              id={isSelected ? "card-focus" : ""}
+              tabIndex={selected === index ? -1 : index}
               className={`text-center outsourcing-cards duration-500 ${
                 selected && index !== selected
-                  ? "w-0 h-0 opacity-0"
+                  ? "md:w-0 md:h-0 md:opacity-0"
                   : index === selected
                   ? "w-full"
-                  : "opacity-100 w-1/4"
+                  : "opacity-100 w-full md:w-[45%]"
               }`}
             >
+              {/* Title */}
               <div className="relative">
                 <div className="h-8 w-1 bg-[#6750A4] absolute rounded-full bottom-1/4" />
                 <h4 className="text-xl capitalize">{position}</h4>
               </div>
+
+              {/* Card */}
               <div
-                className={`mt-2 h-72 rounded-xl bg-[#9A82DB] flex group overflow-hidden ${
-                  index === selected
-                    ? "shadow-2xl shadow-neutral-700 flex-row"
-                    : "shadow-xl flex-col"
-                } relative`}
+                className={`mt-2 rounded-xl flex-col bg-[#9A82DB] flex group overflow-hidden ${
+                  isSelected
+                    ? "min-h-[500px] shadow-2xl lg:shadow-neutral-700 lg:flex-row"
+                    : "shadow-xl min-h-[18rem]"
+                } duration-500 relative`}
               >
+                {/* Image || Avatar */}
                 <div
                   className={`mx-auto border overflow-hidden ${
-                    selected ? "w-1/3 h-full" : "w-24 h-24 rounded-full mt-2"
+                    isSelected
+                      ? "w-full h-32 lg:h-full lg:w-1/3"
+                      : "w-24 h-24 rounded-full mt-2"
                   }`}
                 >
                   <img
@@ -54,19 +69,34 @@ function OutSourcing() {
                     alt={position}
                   />
                 </div>
+
+                {/* Description */}
                 <p className="text-left px-4 py-2">
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                   Inventore repudiandae similique porro aut.
                 </p>
+
+                {/* Button for Desktop */}
                 <button
                   onClick={() => handleButton(index)}
-                  className={`from-transparent py-4 to-[#6750A4] ${
+                  className={`hidden pointer-events-none from-transparent py-4 to-[#6750A4] ${
                     selected
                       ? "absolute top-0 h-full bg-gradient-to-r px-2"
                       : "bg-gradient-to-b"
-                  } mt-auto duration-1000 right-0 lg:opacity-0 group-hover:opacity-100`}
+                  } mt-auto duration-1000 right-0 lg:block lg:pointer-events-auto 
+                  lg:opacity-0 group-hover:opacity-100`}
                 >
                   {selected ? "Close" : "Show more"}
+                </button>
+
+                {/* Button for Smaller Screens */}
+                <button
+                  onClick={() => handleButton(index)}
+                  className="mt-auto p-2 bg-[#E8DEF8] text-[#6750A4] lg:hidden"
+                >
+                  <span className="animate-pulse">
+                    {selected ? "Close" : "Show more"}
+                  </span>
                 </button>
               </div>
             </li>
