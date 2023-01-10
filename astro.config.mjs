b780df322c14a7847f3179ebfcfc4c282ba9dd21
BoJoNVi <bonjomontes@gmail.com>
2023-01-10 17:53:51 +0800

    Implemented foundation for the astro-i18next

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,9 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import { astroImageTools } from "astro-imagetools";
 import svelte from "@astrojs/svelte";
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), astroImageTools, svelte()]
+  integrations: [astroI18next(), tailwind(), mdx(), astroImageTools, svelte()],
 });
