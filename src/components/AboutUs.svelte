512612dba811e0911acaa128104b21775cf22b78
BoJoNVi <bonjomontes@gmail.com>
2023-01-10 11:17:24 +0800

    Implemented responsive design

--- src/components/AboutUs.svelte
+++ src/components/AboutUs.svelte
@@ -17,8 +17,8 @@
 </script>
 
 <ul
-  class={`px-2 relative flex flex-col h-min-[500px] ${
-    selected ? "space-y-" : "-space-y-32"
+  class={`px-2 pb-8 relative flex flex-col h-min-[500px] ${
+    selected ? "" : "space-y-4 md:-space-y-32"
   }`}
 >
   {#each dataList as data, index}
@@ -28,8 +28,8 @@
           ? "shadow-gray-500 w-full p-12"
           : selected
           ? "max-h-0 w-0 p-0 opacity-0 top-0"
-          : "h-full max-h-[500px] w-[50%] p-12 opacity-100"
-      } duration-500 rounded-t-[4rem] rounded-b-xl hover:z-10 even:mx-auto last:ml-auto`}
+          : "h-full max-h-[500px] p-12 opacity-100 md:w-[50%]"
+      } duration-500 rounded-t-[4rem] rounded-b-xl lg:hover:z-10 even:mx-auto last:ml-auto`}
     >
       <h4 class="text-4xl pb-2">{data.title}</h4>
       <p
@@ -38,7 +38,7 @@
         {data.desc}
       </p>
       <button
-        class="opacity-0 block mt-4 underline underline-offset-4 duration-500 group-hover:duration-700 group-hover:opacity-100"
+        class="block mt-4 underline underline-offset-4 duration-500 lg:opacity-0 group-hover:duration-700 group-hover:opacity-100"
         on:click={() => (selected = selected === index + 1 ? null : index + 1)}
       >
         {selected === index + 1 ? "Close" : "Read more >"}
