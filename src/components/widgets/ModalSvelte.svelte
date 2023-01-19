748ad1fb14e56e39d4470823615d57fdd88faf6f
BoJoNVi <bonjomontes@gmail.com>
2023-01-19 17:24:54 +0800

    feat(components): Utilized Svelte to build reusable component
    Implemented reusable modal for the project with Svelte

---
+++ src/components/widgets/ModalSvelte.svelte
@@ -0,0 +1,33 @@
+<script>
+  export let btnClass = "",
+    btnName = "button";
+  let showModal = false;
+</script>
+
+<div>
+  <!-- Button to open the modal -->
+  <button class={btnClass} on:click={() => (showModal = !showModal)}>
+    {btnName}
+  </button>
+
+  <!-- Modal background -->
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <div
+    class={`${
+      showModal
+        ? "opacity-100 pointers-event-auto"
+        : "opacity-0 pointer-events-none"
+    } flex duration-500 fixed top-0 left-0 w-screen h-screen justify-center bg-neutral-700 bg-opacity-50 items-center z-10`}
+    on:click={() => (showModal = !showModal)}
+  >
+    <!-- Modal Container -->
+    <div
+      class={`${
+        showModal ? "translate-y-0" : "translate-y-full"
+      } duration-500 max-w-2xl w-full px-2 mx-auto`}
+    >
+      <!-- Modal Content -->
+      <slot name="content" />
+    </div>
+  </div>
+</div>
