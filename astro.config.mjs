d7aaca3407487f50fcad713648de9f06579bc194
BoJoNVi <bonjomontes@gmail.com>
2023-03-16 10:52:52 +0800

    feat: implemented sitemap to improve SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,16 @@ import mdx from "@astrojs/mdx";
 import { astroImageTools } from "astro-imagetools";
 import svelte from "@astrojs/svelte";
 import astroI18next from "astro-i18next";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  integrations: [astroI18next(), tailwind(), mdx(), astroImageTools, svelte()],
+  site: "https://chatchatabc.com",
+  integrations: [
+    astroI18next(),
+    tailwind(),
+    mdx(),
+    astroImageTools,
+    svelte(),
+    sitemap(),
+  ],
 });
